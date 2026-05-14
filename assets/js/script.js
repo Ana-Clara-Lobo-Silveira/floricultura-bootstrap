9b67450c042e21ab06cc3e717a8f87045254d0b0
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-05-14 11:31:55 -0300

    MODIFY: Finalização do CSS ADD: Eventos no JavaScript. MODIFY: Alterações em classes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,45 @@
       'Você logou com sucesso!', 
       'success');
   });
+
+  document.getElementById('alertComent').addEventListener('click', function() {
+    Swal.fire('Comentário Enviado!', 
+      'Obrigada pelo Feedback!', 
+      'success');
+  });
+
+    document.getElementById('adicionarCarrinho1').addEventListener('click', function() {
+    Swal.fire('Adicionado ao carrinho!', 
+      'Clique em OK para voltar às compras!', 
+      'success');
+  });
+
+  document.getElementById('adicionarCarrinho2').addEventListener('click', function() {
+    Swal.fire('Adicionado ao carrinho!', 
+      'Clique em OK para voltar às compras!', 
+      'success');
+  });
+
+  document.getElementById('adicionarCarrinho3').addEventListener('click', function() {
+    Swal.fire('Adicionado ao carrinho!', 
+      'Clique em OK para voltar às compras!', 
+      'success');
+  });
+
+  document.getElementById('adicionarCarrinho4').addEventListener('click', function() {
+    Swal.fire('Adicionado ao carrinho!', 
+      'Clique em OK para voltar às compras!', 
+      'success');
+  });
+
+  document.getElementById('adicionarCarrinho5').addEventListener('click', function() {
+    Swal.fire('Adicionado ao carrinho!', 
+      'Clique em OK para voltar às compras!', 
+      'success');
+  });
+
+  document.getElementById('adicionarCarrinho6').addEventListener('click', function() {
+    Swal.fire('Adicionado ao carrinho!', 
+      'Clique em OK para voltar às compras!', 
+      'success');
+  });
