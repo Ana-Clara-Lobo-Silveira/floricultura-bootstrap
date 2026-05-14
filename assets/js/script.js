97a919b349adf8c003b59c3d05b30d5c1f531e98
Ana Clara Lobo Silveira <ana.c.silveira15@aluno.senai.br>
2026-05-14 10:02:34 -0300

    ADD: Base do CSS. ADD: Estruturação do footer. MODIFY: Formulário finalizado.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+ // Alerta de Sucesso
+ document.getElementById('alertSucesso').addEventListener('click', function() {
+    Swal.fire('Boas Vindas!', 
+      'Você logou com sucesso!', 
+      'success');
+  });
